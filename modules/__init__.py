fcbbccee3a367ff846a5d1df6bb4151813ceda54
Vendesu <168358183+Vendesu@users.noreply.github.com>
2024-08-15 20:41:49 +0700

    create modules/__init__.py

---
+++ modules/__init__.py
@@ -0,0 +1,17 @@
+from . import (
+    basic, utils, notes, admin, afk, translate, sticker, downloader, spam, info, speedtest, text, help, autotag, ping, status, wellcome, update, statistik, duck
+)
+
+def load_modules(client):
+    modules_list = [
+        basic, utils, notes, admin, afk, translate, sticker, downloader, spam, info, speedtest, text, autotag, ping, status, wellcome, update, statistik, duck
+    ]
+    
+    for module in modules_list:
+        module.load(client)
+        if hasattr(module, 'add_commands'):
+            help.add_module_commands(module.add_commands)
+    
+    help.load(client)
+
+    print("Semua modul berhasil dimuat.")
