698e2bd9d026599fda13a50f6d34fe491bff42e9
Vendesu <168358183+Vendesu@users.noreply.github.com>
2024-08-16 07:23:11 +0700

    update modules/__init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,10 +1,33 @@
-from import (
-    basic, utils, notes, admin, afk, translate, sticker, downloader, spam, info, speedtest, text, help, autotag, ping, status, wellcome, update, statistik, duck, asupan, invgrup
+from . import (
+    basic,
+    utils,
+    notes,
+    admin,
+    afk,
+    translate,
+    sticker,
+    downloader,
+    spam,
+    info,
+    speedtest,
+    text,
+    help,
+    autotag,
+    ping,
+    status,
+    wellcome,
+    update,
+    statistik,
+    duck,
+    invgrup,
+    asupan# Tambahkan modul invgrup di sini
 )
 
 def load_modules(client):
     modules_list = [
-        basic, utils, notes, admin, afk, translate, sticker, downloader, spam, info, speedtest, text, autotag, ping, status, wellcome, update, statistik, duck, asupan, invgrup
+        basic, utils, notes, admin, afk, translate, sticker, downloader, spam,
+        info, speedtest, text, autotag, ping, status, wellcome, update,
+        statistik, duck, invgrup, asupan  # Tambahkan invgrup di sini juga
     ]
     
     for module in modules_list:
@@ -12,6 +35,7 @@ def load_modules(client):
         if hasattr(module, 'add_commands'):
             help.add_module_commands(module.add_commands)
     
+    # Load help module last
     help.load(client)
 
     print("Semua modul berhasil dimuat.")
